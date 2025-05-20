5a25d48bbfc8d6e2e531d0602ee306bf354235dc
gustavo <gusmg90@gmail.com>
2025-05-20 13:06:10 -0600

    api key

--- prueba.php
+++ prueba.php
@@ -3,7 +3,7 @@ $servername = "localhost"; // Dirección del servidor MySQL
 $username = "root"; // Nombre de usuario de la base de datos
 $password = "gustavo_2025"; // Contraseña de la base de datos
 $dbname = "dbprod"; // Nombre de la base de datos
-
+$api_key = "gustavo_2025iusoqieysdlilopoas-lisqpi";
 // Crear conexión
 $conn = new mysqli($servername, $username, $password, $dbname);
 
